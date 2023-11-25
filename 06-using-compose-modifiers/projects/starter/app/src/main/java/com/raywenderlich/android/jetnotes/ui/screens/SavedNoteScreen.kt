ac1567eade41b66b8b0b7550884a48f2eb03c8ce
Luis Morales <joseluis.morales@redsinergia.com>
2023-11-25 17:41:38 -0600

    finished

--- 06-using-compose-modifiers/projects/starter/app/src/main/java/com/raywenderlich/android/jetnotes/ui/screens/SavedNoteScreen.kt
+++ 06-using-compose-modifiers/projects/starter/app/src/main/java/com/raywenderlich/android/jetnotes/ui/screens/SavedNoteScreen.kt
@@ -1,17 +1,24 @@
 package com.raywenderlich.android.jetnotes.ui.screens
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import com.raywenderlich.android.jetnotes.data.database.model.ColorDbModel
 import com.raywenderlich.android.jetnotes.domain.model.ColorModel
 import com.raywenderlich.android.jetnotes.ui.components.NoteColor
 import com.raywenderlich.android.jetnotes.util.fromHex
@@ -52,5 +59,44 @@ fun ColorItemPreview() {
         onColorSelect = { colorModel: ColorModel ->
 
         }
+    )
+}
+
+@Composable
+fun ColorPicker(
+    colors: List<ColorModel>,
+    onColorSelect: (ColorModel) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = "Color Picker",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(8.dp)
+        )
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            items(colors.size) { itemIndex ->
+                val color = colors[itemIndex]
+                ColorItem(colorModel = color, onColorSelect = onColorSelect)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ColorPickerPreview() {
+    ColorPicker(
+        colors = listOf(
+            ColorModel.DEFAULT,
+            ColorModel.DEFAULT,
+            ColorModel.DEFAULT
+        ),
+        onColorSelect = { }
     )
 }
