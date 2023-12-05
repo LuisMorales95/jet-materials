7772d984efbc213e1a4ef481b6c3873fba54db1a
luis morales <joseluismoralesp95@gmail.com>
2023-12-04 23:45:52 -0600

    Using Material Composable in Note

--- 08-applying-material-design-to-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetnotes/ui/components/Note.kt
+++ 08-applying-material-design-to-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetnotes/ui/components/Note.kt
@@ -31,97 +31,89 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package com.yourcompany.android.jetnotes.ui.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Checkbox
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ListItem
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.yourcompany.android.jetnotes.domain.model.NoteModel
+import com.yourcompany.android.jetnotes.theme.JetNotesTheme
 import com.yourcompany.android.jetnotes.util.fromHex
-import androidx.compose.foundation.clickable
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun Note(
-  note: NoteModel,
-  onNoteClick: (NoteModel) -> Unit = {},
-  onNoteCheckedChange: (NoteModel) -> Unit = {},
-  isSelected: Boolean
+	note: NoteModel,
+	onNoteClick: (NoteModel) -> Unit = {},
+	onNoteCheckedChange: (NoteModel) -> Unit = {},
+	isSelected: Boolean = false
 ) {
-  val backgroundShape: Shape = RoundedCornerShape(4.dp)
-
-  Row(
-    modifier = Modifier
-      .padding(8.dp)
-      .shadow(1.dp, backgroundShape)
-      .fillMaxWidth()
-      .heightIn(min = 64.dp)
-      .background(Color.White, backgroundShape)
-      .clickable(onClick = { onNoteClick(note) })
-  ) {
-    NoteColor(
-      modifier = Modifier
-        .align(Alignment.CenterVertically)
-        .padding(start = 16.dp, end = 16.dp),
-      color = Color.fromHex(note.color.hex),
-      size = 40.dp,
-      border = 1.dp
-    )
-    Column(
-      modifier = Modifier
-        .weight(1f)
-        .align(Alignment.CenterVertically)
-    ) {
-      Text(
-        text = note.title,
-        color = Color.Black,
-        maxLines = 1,
-        style = TextStyle(
-          fontWeight = FontWeight.Normal,
-          fontSize = 16.sp,
-          letterSpacing = 0.15.sp
-        )
-      )
-      Text(
-        text = note.content,
-        color = Color.Black.copy(alpha = 0.75f),
-        maxLines = 1,
-        style = TextStyle(
-          fontWeight = FontWeight.Normal,
-          fontSize = 14.sp,
-          letterSpacing = 0.25.sp
-        )
-      )
+	val background = if (isSelected) {
+        Color.LightGray
+    } else {
+        MaterialTheme.colors.surface
     }
-    if (note.isCheckedOff != null) {
-      Checkbox(
-        checked = note.isCheckedOff,
-        onCheckedChange = { isChecked ->
-          val newNote = note.copy(isCheckedOff = isChecked)
-          onNoteCheckedChange(newNote)
-        },
-        modifier = Modifier
-          .padding(16.dp)
-          .align(Alignment.CenterVertically)
-      )
-    }
-  }
+	
+	Card(
+		shape = RoundedCornerShape(4.dp),
+		modifier = Modifier
+			.padding(8.dp)
+			.fillMaxWidth(),
+		backgroundColor = background,
+		content = {
+			ListItem(
+				text = {
+					Text(text = note.title, maxLines = 1)
+				},
+				secondaryText = {
+					Text(text = note.content, maxLines = 1)
+				},
+				icon = {
+					NoteColor(
+						color = Color.fromHex(note.color.hex),
+						size = 40.dp,
+						border = 1.dp
+					)
+				},
+				trailing = {
+					if (note.isCheckedOff != null) {
+						Checkbox(
+							checked = note.isCheckedOff,
+							onCheckedChange = { isChecked ->
+								val newNote = note.copy(isCheckedOff = isChecked)
+								onNoteCheckedChange(newNote)
+							},
+							modifier = Modifier
+								.padding(start = 16.dp)
+						)
+					}
+				},
+				modifier = Modifier.clickable {
+					onNoteClick.invoke(note)
+				}
+			)
+		}
+	)
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun NotePreview() {
-  Note(note = NoteModel(1, "Note 1", "Content 1", null), isSelected = false)
+	JetNotesTheme {
+		Note(note = NoteModel(1, "Note 1", "Content 1", null), isSelected = false)
+	}
 }
