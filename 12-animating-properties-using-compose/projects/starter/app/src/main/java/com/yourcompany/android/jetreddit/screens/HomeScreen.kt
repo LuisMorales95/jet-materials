8ff2cdfee95c04f7ddceaad69d4826bef4284c66
Luis Morales <joseluis.morales@redsinergia.com>
2024-01-03 22:08:52 -0600

    Chapter 12: Animating Properties Using Compose Done

--- 12-animating-properties-using-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetreddit/screens/HomeScreen.kt
+++ 12-animating-properties-using-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetreddit/screens/HomeScreen.kt
@@ -34,35 +34,65 @@
 package com.yourcompany.android.jetreddit.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.yourcompany.android.jetreddit.components.ImagePost
+import com.yourcompany.android.jetreddit.components.JoinedToast
 import com.yourcompany.android.jetreddit.components.TextPost
 import com.yourcompany.android.jetreddit.domain.model.PostModel
 import com.yourcompany.android.jetreddit.domain.model.PostType
 import com.yourcompany.android.jetreddit.viewmodel.MainViewModel
+import java.util.Timer
+import kotlin.concurrent.timerTask
 
 @Composable
 fun HomeScreen(viewModel: MainViewModel) {
-  val posts: List<PostModel> by viewModel.allPosts.observeAsState(listOf())
+    val posts: List<PostModel> by viewModel.allPosts.observeAsState(listOf())
 
-  LazyColumn(
-    modifier = Modifier.background(color = MaterialTheme.colors.secondary)
-  ) {
-    items(posts) {
-      if (it.type == PostType.TEXT) {
-        TextPost(it)
-      } else {
-        ImagePost(it)
-      }
-      Spacer(modifier = Modifier.height(6.dp))
+    var isToastVisible by remember {
+        mutableStateOf(false)
     }
-  }
+
+    val onJoinClickAction: (Boolean) -> Unit = { joined ->
+        isToastVisible = joined
+        if (isToastVisible) {
+            Timer().schedule(timerTask {
+                isToastVisible = false
+            }, 3000)
+        }
+    }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(
+            modifier = Modifier.background(color = MaterialTheme.colors.secondary)
+        ) {
+            items(posts) {
+                if (it.type == PostType.TEXT) {
+                    TextPost(it, onJoinClickAction)
+                } else {
+                    ImagePost(it, onJoinClickAction)
+                }
+                Spacer(modifier = Modifier.height(6.dp))
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 16.dp)
+        ) {
+            JoinedToast(visible = isToastVisible)
+        }
+    }
 }
