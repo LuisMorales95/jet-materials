03b5fe980b94bf87186580fcc9e4702fb142d91a
Luis Morales <joseluis.morales@redsinergia.com>
2024-01-07 17:53:45 -0600

    Chapter 13 UI Framework compatibility with compose Done!

--- 13-adding-view-compatibility/projects/starter/app/src/main/java/com/yourcompany/android/jetreddit/screens/HomeScreen.kt
+++ 13-adding-view-compatibility/projects/starter/app/src/main/java/com/yourcompany/android/jetreddit/screens/HomeScreen.kt
@@ -33,59 +33,231 @@
  */
 package com.yourcompany.android.jetreddit.screens
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.yourcompany.android.jetreddit.R
 import com.yourcompany.android.jetreddit.components.ImagePost
 import com.yourcompany.android.jetreddit.components.JoinedToast
 import com.yourcompany.android.jetreddit.components.TextPost
 import com.yourcompany.android.jetreddit.domain.model.PostModel
 import com.yourcompany.android.jetreddit.domain.model.PostType
 import com.yourcompany.android.jetreddit.viewmodel.MainViewModel
+import com.yourcompany.android.jetreddit.views.TrendingTopicView
 import java.util.*
 import kotlin.concurrent.schedule
 
+private val trendingItems = listOf(
+    TrendingTopicModel(
+        "Compose Tutorial",
+        R.drawable.jetpack_composer
+    ),
+    TrendingTopicModel(
+        "Compose Animations",
+        R.drawable.jetpack_compose_animations
+    ),
+    TrendingTopicModel(
+        "Compose Migration",
+        R.drawable.compose_migration_crop
+    ),
+    TrendingTopicModel(
+        "DataStore Tutorial",
+        R.drawable.data_storage
+    ),
+    TrendingTopicModel(
+        "Android Animations",
+        R.drawable.android_animations
+    ),
+    TrendingTopicModel(
+        "Deep Links in Android",
+        R.drawable.deeplinking
+    )
+)
 @Composable
 fun HomeScreen(viewModel: MainViewModel) {
-  val posts: List<PostModel>
-      by viewModel.allPosts.observeAsState(listOf())
+    val posts: List<PostModel>
+        by viewModel.allPosts.observeAsState(listOf())
 
-  var isToastVisible by remember { mutableStateOf(false) }
+    var isToastVisible by remember { mutableStateOf(false) }
 
-  val onJoinClickAction: (Boolean) -> Unit = { joined ->
-    isToastVisible = joined
-    if (isToastVisible) {
-      Timer().schedule(3000) {
-        isToastVisible = false
-      }
-    }
-  }
-
-  Box(modifier = Modifier.fillMaxSize()) {
-    LazyColumn(modifier = Modifier.background(color = MaterialTheme.colors.secondary)) {
-      items(posts) {
-        if (it.type == PostType.TEXT) {
-          TextPost(it, onJoinButtonClick = onJoinClickAction)
-        } else {
-          ImagePost(it, onJoinButtonClick = onJoinClickAction)
+    val onJoinClickAction: (Boolean) -> Unit = { joined ->
+        isToastVisible = joined
+        if (isToastVisible) {
+            Timer().schedule(3000) {
+                isToastVisible = false
+            }
         }
-        Spacer(modifier = Modifier.height(6.dp))
-      }
     }
 
-    Box(
-      modifier = Modifier
-        .align(Alignment.BottomCenter)
-        .padding(bottom = 16.dp)
-    ) {
-      JoinedToast(visible = isToastVisible)
+    val homeScreenItems = mapHomeScreenItems(posts)
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(
+            modifier = Modifier
+                .background(color = MaterialTheme.colors.secondary)
+        ) {
+            items(homeScreenItems) {
+                if (it.type == HomeScreenItemType.TRENDING) {
+                    TrendingTopics(
+                        trendingTopics = trendingItems,
+                        modifier = Modifier.padding(top = 16.dp, bottom = 6.dp)
+                    )
+                } else if (it.post != null) {
+                    val post = it.post
+                    if (post.type == PostType.TEXT) {
+                        TextPost(
+                            post = post,
+                            onJoinButtonClick = onJoinClickAction
+                        )
+                    } else {
+                        ImagePost(
+                            post = post,
+                            onJoinButtonClick = onJoinClickAction
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(6.dp))
+                }
+            }
+        }
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 16.dp)
+        ) {
+            JoinedToast(visible = isToastVisible)
+        }
     }
-  }
 }
+
+private fun mapHomeScreenItems(
+    posts: List<PostModel>
+): List<HomeScreenItem> {
+    val homeScreenItems = mutableListOf<HomeScreenItem>()
+
+    homeScreenItems.add(
+        HomeScreenItem(HomeScreenItemType.TRENDING)
+    )
+
+    posts.forEach { post ->
+        homeScreenItems.add(
+            HomeScreenItem(HomeScreenItemType.POST, post)
+        )
+    }
+
+    return homeScreenItems
+}
+
+@Composable
+private fun TrendingTopics(
+    trendingTopics: List<TrendingTopicModel>,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        shape = MaterialTheme.shapes.large,
+        modifier = modifier
+    ) {
+        Column(modifier = Modifier.padding(vertical = 8.dp)) {
+            Row(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Star,
+                    contentDescription = "Star Icon",
+                    tint = Color.Blue,
+                    modifier = Modifier.size(18.dp)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = "Trending Today",
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+            LazyRow(
+                contentPadding = PaddingValues(
+                    start = 16.dp,
+                    top = 8.dp,
+                    end = 16.dp
+                ),
+                content = {
+                    itemsIndexed(
+                        items = trendingTopics,
+                        itemContent = {index, trendingModel ->
+                            TrendingTopic(trendingTopicModel = trendingModel)
+                            if (index != trendingTopics.lastIndex) {
+                                Spacer(modifier = Modifier.width(8.dp))
+                            }
+                        }
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TrendingTopicsPreview() {
+    TrendingTopics(
+        trendingTopics = trendingItems
+    )
+}
+
+@Composable
+private fun TrendingTopic(trendingTopicModel: TrendingTopicModel) {
+    AndroidView(factory = { context ->
+        TrendingTopicView(context).apply {
+            text = trendingTopicModel.text
+            image = trendingTopicModel.imageRes
+        }
+    })
+}
+
+@Preview
+@Composable
+fun TrendingTopicPreview() {
+    TrendingTopic(
+        trendingTopicModel = TrendingTopicModel(
+            text = "Compose Animations",
+            imageRes = R.drawable.jetpack_compose_animations
+        )
+    )
+}
+
+private data class HomeScreenItem(
+    val type: HomeScreenItemType,
+    val post: PostModel? = null
+)
+
+private enum class HomeScreenItemType {
+    TRENDING,
+    POST
+}
+
+private data class TrendingTopicModel(
+    val text: String,
+    @DrawableRes
+    val imageRes: Int = 0
+)
